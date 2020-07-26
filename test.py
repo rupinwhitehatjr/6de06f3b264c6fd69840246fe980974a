970360b1315ec644484267728a33ba63f5d4fc88
Rupin <rupin@whitehatjr.com>
2020-07-26 12:33:10 +0530

    master

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #print("a")
 import xlrd
+import os
 
 
 def getDownloadFileURL(url):
@@ -12,43 +13,91 @@ def getDownloadFileURL(url):
 
 	fileID=url
 	return "https://drive.google.com/uc?export=download&id="+fileID
+def formatText(textData):
+	
+	textData=str(textData)	
+	if(textData.isnumeric()):
+		textData=int(textData)
+	textData=textData.strip()
+	
+	
+	return textData
 
 
 def main():
+	xlFile=xlrd.open_workbook("QuizQuestions.xlsx")
+	indexTemplatefile = open("indexTemplate.html", "r")
+	indexhtml=indexTemplatefile.read()
+	indexTemplatefile.close()
+
+	with open('index.html', 'wb') as temp_file:
+			temp_file.write(bytes(indexhtml, 'utf-8'))
+	
+	#templateFile = open("template.html", "r")
+	#templateHTML=templateFile.read()
+	index=0
+	for sheetname in xlFile.sheet_names():
+		if not os.path.exists(sheetname):
+			os.mkdir(sheetname)
+		generateHTMLFiles(xlFile,index,sheetname)
+		updateIndexFile(sheetname)
+		index=index+1
+
+def updateIndexFile(lsheetname):
+	indexfile = open("index.html", "r")
+	indexhtml=indexfile.read()
+	indexfile.close()
+
+	buttonTemplate = open("buttonTemplate.html", "r")
+	buttonHTML=buttonTemplate.read()
+	buttonTemplate.close()
+
+
+	buttonHTML=buttonHTML.replace("#link", lsheetname+'/1.html')
+	buttonHTML=buttonHTML.replace("#sheetname", lsheetname)
+
+	newindexhtml=indexhtml.replace("#nextbutton",buttonHTML)
+	with open('index.html', 'wb') as temp_file:
+			temp_file.write(bytes(newindexhtml, 'utf-8'))
+
+
+
+
+def generateHTMLFiles(workbook, sheetIndex,foldername):
 	templateFile = open("template.html", "r")
 	templateHTML=templateFile.read()
 	templateFile.close()
 
-	xlFile=xlrd.open_workbook("ADV.xlsx")  
-	#print(xlFile.sheet_names())
-	rows=295
-	sheet = xlFile.sheet_by_index(0)     
-	      
+	
+	sheet = workbook.sheet_by_index(sheetIndex)     
+	rows=sheet.nrows
+	#print(rows)
 	# For row 0 and column 0     
-	print(sheet.cell_value(0, 0))
+	#print(sheet.cell_value(0, 0))
 
-	for row in range(1,295):
-		srno=sheet.cell_value(row, 0)
-		level=sheet.cell_value(row, 1)
-		#print(level)
-		version=sheet.cell_value(row, 2)
-		classNumber=int(sheet.cell_value(row, 3)) 
-		question_number=int(sheet.cell_value(row, 4)) 
-		category=sheet.cell_value(row, 5)
-		question_text=sheet.cell_value(row, 6)
-		question_image=sheet.cell_value(row, 7)
+	for row in range(1,rows):
+		srno=formatText(sheet.cell_value(row, 0))
+		level=formatText(sheet.cell_value(row, 1))
+		print(row)
+		
+		version=formatText(sheet.cell_value(row, 2))
+		classNumber=formatText(sheet.cell_value(row, 3)) 
+		question_number=formatText(sheet.cell_value(row, 4)) 
+		category=formatText(sheet.cell_value(row, 5))
+		question_text=formatText(sheet.cell_value(row, 6))
+		question_image=formatText(sheet.cell_value(row, 7))
 
 		optionAImage=sheet.cell_value(row, 8)
-		optionAText=sheet.cell_value(row, 9)
+		optionAText=formatText(sheet.cell_value(row, 9))
 
 		optionBImage=sheet.cell_value(row, 10)
-		optionBText=sheet.cell_value(row, 11)
+		optionBText=formatText(sheet.cell_value(row, 11))
 
 		optionCImage=sheet.cell_value(row, 12)
-		optionCText=sheet.cell_value(row, 13)
+		optionCText=formatText(sheet.cell_value(row, 13))
 
 		optionDImage=sheet.cell_value(row, 14)
-		optionDText=sheet.cell_value(row, 15)
+		optionDText=formatText(sheet.cell_value(row, 15))
 		answer=sheet.cell_value(row, 16)
 
 		
@@ -59,7 +108,7 @@ def main():
 		else:
 			previousfileName="index.html"	
 		nextFileName=str(row+1)+".html"
-		outfile=open(outputfileName, 'w')
+		#outfile=open(outputfileName, 'w')
 		htmlData=templateHTML
 
 		htmlData=htmlData.replace("#QuestionNumber", str(question_number))
@@ -84,8 +133,10 @@ def main():
 		htmlData=htmlData.replace("#AnswerOption", answer)
 		htmlData=htmlData.replace("#previousLink", previousfileName)
 		htmlData=htmlData.replace("#nextLink", nextFileName)
-		outfile.write(htmlData)
-		outfile.close()
+		with open(os.path.join(foldername, outputfileName), 'wb') as temp_file:
+			temp_file.write(bytes(htmlData, 'utf-8'))
+		#outfile.write(htmlData)
+		#outfile.close()
 	
 
 main()
